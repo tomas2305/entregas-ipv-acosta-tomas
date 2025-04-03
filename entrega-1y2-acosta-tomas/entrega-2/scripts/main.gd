0e45d492c46ece0f74cf27345cbadb58017a793d
Tomás Acosta <tomas23058@gmail.com>
2025-04-02 22:09:30 -0300

    feat: random turrets position

--- entrega-1y2-acosta-tomas/entrega-2/scripts/main.gd
+++ entrega-1y2-acosta-tomas/entrega-2/scripts/main.gd
@@ -1,7 +1,18 @@
 extends Node2D
 
+@export var turret_scene : PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var screen_size = get_viewport_rect().size
+	var half_screen_y = screen_size.y / 2
+	
 	$Player.set_projectile_container(self)
-	$Turret.set_values($Player, self)
+	for i in range(3):
+		var turret = turret_scene.instantiate()
+		add_child(turret)
+		turret.set_values($Player, self)
+		var random_x = randi_range(50, screen_size.x - 50)
+		var random_y = randi_range(50, half_screen_y - 50)
+		
+		turret.position = Vector2(random_x, random_y)
